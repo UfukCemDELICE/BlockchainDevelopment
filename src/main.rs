0e451dbac099e90d6bc1b6944317d94b2401688d
UfukCemDELICE <ufukcemdelice02@gmail.com>
2022-09-21 22:42:27 +0300

    Mining Added

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use blockchainlib::*;
 fn main () {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis Block".to_owned());
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis Block".to_owned(), 0x00000000ffffffffffffffffffffffffff);
 
-    println!("{:?}", block);
+    block.hash = block.hash();
 
-    let h = block.hash();
+    println!("{:?}", &block);
 
-    print!("{:?}", &h);
-
-    block.hash = h;
-
-    println!("{:?}", block);
+    block.mine();
+    
+    println!("{:?}", &block);
 }
